d5cf386df56f208cf647c4fac976285ffadf69bb
n-lebel <nicklbch@gmail.com>
2023-04-01 10:35:43 +0100

    update to 0.14

--- methods/guest/src/bin/search_json.rs
+++ methods/guest/src/bin/search_json.rs
@@ -16,31 +16,8 @@
 
 use json::parse;
 use json_core::Outputs;
-use risc0_zkvm::guest::{env, sha};
+use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
-pub fn main() {
-    let data1: String = env::read();
-    let data2: String = env::read();
-
-    let sha1 = sha::digest(&data1.as_bytes());
-    let sha2 = sha::digest(&data2.as_bytes());
-
-    let data1 = parse(&data1).unwrap();
-    let data2 = parse(&data2).unwrap();
-
-    let proven_val1 = data1["critical_data"].as_u32().unwrap();
-    let proven_val2 = data2["critical_data"].as_u32().unwrap();
-
-    if proven_val1 != proven_val2 {
-        panic!("Values of critical_data do not agree!");
-    }
-
-    let out = Outputs {
-        hash1: *sha1,
-        hash2: *sha2,
-    };
-
-    env::commit(&out);
-}
+pub fn main() {}
