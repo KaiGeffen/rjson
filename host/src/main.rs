c99402e19cee2d35ee619ea41dd1d8b648354c66
n-lebel <nicklbch@gmail.com>
2023-04-01 10:52:24 +0100

    clean slate

--- host/src/main.rs
+++ host/src/main.rs
@@ -19,38 +19,4 @@ use methods::{SEARCH_JSON_ELF, SEARCH_JSON_ID};
 use risc0_zkvm::serde::{from_slice, to_vec};
 use risc0_zkvm::Prover;
 
-fn main() {
-    // Load data 1
-    let mut file1 =
-        std::fs::File::open("res/example1.json").expect("Example file should be accessible");
-    let mut data1 = String::new();
-    file1.read_to_string(&mut data1)
-        .expect("Should not have I/O errors");
-
-    // Load data 2
-    let mut file2 =
-        std::fs::File::open("res/example2.json").expect("Example file should be accessible");
-    let mut data2 = String::new();
-    file2.read_to_string(&mut data2)
-        .expect("Should not have I/O errors");
-
-    // Make the prover.
-    let mut prover = Prover::new(SEARCH_JSON_ELF, SEARCH_JSON_ID)
-        .expect("Prover should be constructed from matching method code & ID");
-
-    // Add both json data
-    prover.add_input_u32_slice(&to_vec(&data1).expect("should be serializable"));
-    prover.add_input_u32_slice(&to_vec(&data2).expect("should be serializable"));
-
-    // Run prover & generate receipt
-    let receipt = prover.run().expect("Code should be provable");
-
-    receipt
-        .verify(SEARCH_JSON_ID)
-        .expect("Proven code should verify");
-
-    let journal = &receipt.journal;
-    let outputs: Outputs = from_slice(&journal).expect("Journal should contain an Outputs object");
-
-    println!("\nThe following JSON files agree on the value of 'critical_data':\n{}\n{}\n", outputs.hash1, outputs.hash2);
-}
+fn main() {}
